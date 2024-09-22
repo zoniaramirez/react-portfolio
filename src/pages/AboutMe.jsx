da9858ad3f2113a80634d8885207695d51c4cefc
Zonia Ramirez <jslnramirez@gmail.com>
2024-09-22 18:11:48 -0400

    updated AboutMe

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -2,8 +2,10 @@ export default function AboutMe() {
   return (
     <section>
       <h2>About Me</h2>
-      <img src="/path/to/photo.jpg" alt="Developer" />
-      <p>Short bio about the developer.</p>
+      <img src="/src/assets/avatar.png" alt="Developer" />
+      {/*eslint-disable-next-line react/no-unescaped-entities */}
+      <p>Hello! I'm Zonia Ramirez, a passionate and dedicated junior web developer with a background in creating dynamic and responsive web applications. With a keen eye for detail and a commitment to delivering high-quality code, I specialize in front-end development using modern technologies such as React, JavaScript, HTML, and CSS. I thrive in collaborative environments and am always eager to learn and adapt to new challenges. Let's build something amazing together!
+      </p>
     </section>
   );
 }
