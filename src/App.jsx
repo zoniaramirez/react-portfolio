52abe87cc112dc6012b32d18a5461057e8293add
Zonia Ramirez <jslnramirez@gmail.com>
2024-09-22 17:17:24 -0400

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 // Bringing in the required import from 'react-router-dom'
 import { Outlet } from 'react-router-dom';
-import Nav from './components/NavTabs';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
   return (
     <>
-      <Nav />
+      <Header />
       <main className="mx-3">
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 }
