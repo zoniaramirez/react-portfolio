debe5f15d320bc40b83fe08bf940cc9afc326449
Zonia Ramirez <jslnramirez@gmail.com>
2024-09-22 17:15:14 -0400

    updated pages

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,15 +1,15 @@
+import Project from '../components/Project';
+
 export default function Portfolio() {
   return (
-    <div>
-      <h1>Portfolio</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
-    </div>
-  );
+    <section>
+    <h2>Portfolio</h2>
+    <Project title="Project 1" image="/path/to/image1.jpg" deployedLink="#" repoLink="#" />
+    <Project title="Project 2" image="/path/to/image2.jpg" deployedLink="#" repoLink="#" />
+    <Project title="Project 3" image="/path/to/image3.jpg" deployedLink="#" repoLink="#" />
+    <Project title="Project 4" image="/path/to/image4.jpg" deployedLink="#" repoLink="#" />
+    <Project title="Project 5" image="/path/to/image5.jpg" deployedLink="#" repoLink="#" />
+    <Project title="Project 6" image="/path/to/image6.jpg" deployedLink="#" repoLink="#" />
+  </section>
+);
 }
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,19 +1,13 @@
 export default function Resume() {
   return (
-    <div>
-      <h1>Resume Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
-    </div>
-  );
+    <section>
+    <h2>Resume</h2>
+    <a href="/path/to/resume.pdf" download>Download Resume</a>
+    <ul>
+      <li>Proficiency 1</li>
+      <li>Proficiency 2</li>
+      <li>Proficiency 3</li>
+    </ul>
+  </section>
+);
 }
