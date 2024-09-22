9939be33a9e11e4fd9fdfac008473590a220762f
Zonia Ramirez <jslnramirez@gmail.com>
2024-09-22 18:23:34 -0400

    updated AboutMe and Resume

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -4,7 +4,7 @@ export default function AboutMe() {
       <h2>About Me</h2>
       <img src="/src/assets/avatar.png" alt="Developer" />
       {/*eslint-disable-next-line react/no-unescaped-entities */}
-      <p>Hello! I'm Zonia Ramirez, a passionate and dedicated junior web developer with a background in creating dynamic and responsive web applications. With a keen eye for detail and a commitment to delivering high-quality code, I specialize in front-end development using modern technologies such as React, JavaScript, HTML, and CSS. I thrive in collaborative environments and am always eager to learn and adapt to new challenges. Let's build something amazing together!
+      <p>Hello! I'm Zonia Ramirez, a passionate and dedicated junior web developer with a background in creating dynamic and responsive web applications. With a keen eye for detail and a commitment to delivering high-quality code, I specialize in front-end development using modern technologies such as React, JavaScript, HTML, jQuery, Bootstrap, and CSS. I also specialize in back-end development using technologies such as APIs, Node, Express, SQL, Sequelize, MongoDB, Mongoose, REST, and GraphQL. I thrive in collaborative environments and am always eager to learn and adapt to new challenges. Let's build something amazing together!
       </p>
     </section>
   );
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,9 +4,23 @@ export default function Resume() {
     <h2>Resume</h2>
     <a href="/path/to/resume.pdf" download>Download Resume</a>
     <ul>
-      <li>Proficiency 1</li>
-      <li>Proficiency 2</li>
-      <li>Proficiency 3</li>
+      <h2>Front-end Proficiencies</h2>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>jQuery</li>
+      <li>Responsive Design</li>
+      <li>React</li>
+      <li>Bootstrap</li>
+    </ul>
+    <ul>
+      <h2>Back-end Proficiencies</h2>
+      <li>APIs</li>
+      <li>Node</li>
+      <li>Express</li>
+      <li>SQL, Sequelize</li>
+      <li>MongoDB, Mongoose</li>
+      <li>REST</li>
+      <li>GraphQL</li>
     </ul>
   </section>
 );
