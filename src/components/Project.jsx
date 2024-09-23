4915e820ec40f7ff3f952db3414abff512900733
Zonia Ramirez <jslnramirez@gmail.com>
2024-09-22 20:39:25 -0400

    updated portfolio

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,10 +2,14 @@
 const Project = ({ title, image, deployedLink, repoLink }) => (
     <div>
       <h3>{title}</h3>
-      <img src={image} alt={title} />
-      <p><a href={deployedLink}>Deployed Application</a></p>
-      <p><a href={repoLink}>GitHub Repository</a></p>
-    </div>
+      <img src={image} alt={title} style={{ width: '200px', height: 'auto' }} />
+      <p>
+      <a href={deployedLink} target="_blank" rel="noopener noreferrer">Deployed Application or Video Link</a>
+    </p>
+    <p>
+      <a href={repoLink} target="_blank" rel="noopener noreferrer">GitHub Repository</a>
+    </p>
+  </div>
   );
   
   export default Project;
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -4,12 +4,12 @@ export default function Portfolio() {
   return (
     <section>
     <h2>Portfolio</h2>
-    <Project title="Project 1" image="/path/to/image1.jpg" deployedLink="#" repoLink="#" />
-    <Project title="Project 2" image="/path/to/image2.jpg" deployedLink="#" repoLink="#" />
-    <Project title="Project 3" image="/path/to/image3.jpg" deployedLink="#" repoLink="#" />
-    <Project title="Project 4" image="/path/to/image4.jpg" deployedLink="#" repoLink="#" />
-    <Project title="Project 5" image="/path/to/image5.jpg" deployedLink="#" repoLink="#" />
-    <Project title="Project 6" image="/path/to/image6.jpg" deployedLink="#" repoLink="#" />
+    <Project title="Task Board" image="/src/assets/task-board.png" deployedLink="https://zoniaramirez.github.io/task-board/" repoLink="https://github.com/zoniaramirez/task-board" />
+    <Project title="Weather Dashboard" image="/src/assets/Weather-Dashboard.png" deployedLink="https://zoniaramirez.github.io/weather-dashboard/" repoLink="https://github.com/zoniaramirez/weather-dashboard" />
+    <Project title="Employee Tracker" image="/src/assets/Employeetracker.png" deployedLink="https://drive.google.com/file/d/1Z-LLrQIwCABRCT7xE_iRyK9PLM7gFsfq/view?usp=drive_link" repoLink="https://github.com/zoniaramirez/employee-tracker" />
+    <Project title="noSQL" image="/src/assets/nosql.png" deployedLink="https://drive.google.com/file/d/1zvkd9_dRS_QF4gKVVaHzGDrs2ScbfyUI/view?usp=sharing" repoLink="https://github.com/zoniaramirez/social-network-API" />
+    <Project title="Note Taker" image="/src/assets/Note-Taker.png" deployedLink="https://note-taker-lk75.onrender.com" repoLink="https://github.com/zoniaramirez/note-taker" />
+    <Project title="Random Drink Recipes" image="/src/assets/project1.png" deployedLink="https://meg-an321.github.io/drink-recipes/" repoLink="https://github.com/meg-an321/drink-recipes" />
   </section>
 );
 }
