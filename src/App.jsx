253e2e41fee4bcd69e23926a50895641c26b870f
Zonia Ramirez <jslnramirez@gmail.com>
2024-09-23 19:57:00 -0400

    updated footer and app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,33 @@ function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
   return (
     <>
+       <style>
+      {`
+        html, body {
+          height: 100%;
+          margin: 0;
+        }
+        .min-vh-100 {
+          min-height: 100vh;
+        }
+        .d-flex {
+          display: flex;
+        }
+        .flex-column {
+          flex-direction: column;
+        }
+        .mt-auto {
+          margin-top: auto;
+        }
+      `}
+    </style>
+    <div className="d-flex flex-column min-vh-100">
       <Header />
       <main className="mx-3">
         <Outlet />
       </main>
       <Footer />
+      </div>
     </>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,13 @@
 // src/components/Footer.jsx
 const Footer = () => (
-  <footer>
-    <p><a href="https://github.com/username">GitHub</a></p>
-    <p><a href="https://linkedin.com/in/username">LinkedIn</a></p>
-    <p><a href="https://stackoverflow.com/users/username">Stack Overflow</a></p>
+  <footer className="bg-dark text-white py-4 mt-auto">
+    <div className="container text-center">
+      <div className="d-flex justify-content-center mb-2 flex-wrap">
+        <a href="https://github.com/username" className="text-white mx-3 my-1">GitHub</a>
+        <a href="https://linkedin.com/in/username" className="text-white mx-3 my-1">LinkedIn</a>
+        <a href="https://stackoverflow.com/users/username" className="text-white mx-3 my-1">Stack Overflow</a>
+      </div>
+    </div>
   </footer>
 );
 
