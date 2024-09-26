33253bd66567d2313c507c5100799629fab66a1d
Zonia Ramirez <jslnramirez@gmail.com>
2024-09-25 21:49:01 -0400

    updated typo in readme

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -8,7 +8,7 @@ export default function AboutMe() {
         </div>
         <div className="col-md-8">
           {/*eslint-disable-next-line react/no-unescaped-entities */}
-          <p>Hello! I'm Zonia Ramirez, a passionate and dedicated junior web developer with a background in creating dynamic and responsive web applications. With a keen eye for detail and a commitment to delivering high-quality code, I specialize in front-end development using modern technologies such as React, JavaScript, HTML, jQuery, Bootstrap, and CSS. I also specialize in back-end development using technologies such as APIs, Node, Express, SQL, Sequelize, MongoDB, Mongoose, REST, and GraphQL. I thrive in collaborative environments and am always eager to learn and adapt to new challenges. Let's build something amazing together!
+          <p>Hello! I'm Zonia Ramirez, a passionate and dedicated junior web developer with a background in creating dynamic and responsive web applications. With a keen eye for detail and a commitment to delivering high-quality code, I specialize in front-end development using modern technologies such as React, JavaScript, HTML, jQuery, Bootstrap, and CSS. I also specialize in back-end development using technologies such as APIs, Node, Express, SQL, Sequelize, MongoDB, Mongoose, REST, and GraphQL. I thrive in collaborative environments and I am always eager to learn and adapt to new challenges. Let's build something amazing together!
           </p>
         </div>
       </div>
