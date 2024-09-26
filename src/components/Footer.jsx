43139ec7c338f5e89a865600cd2af84e7a43ba9b
Zonia Ramirez <jslnramirez@gmail.com>
2024-09-25 21:37:23 -0400

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => (
       <div className="d-flex justify-content-center mb-2 flex-wrap">
         <a href="https://github.com/zoniaramirez" target="_blank" rel="noopener noreferrer" className="text-white mx-3 my-1">GitHub</a>
         <a href="https://linkedin.com/in/zonia-ramirez-59172364" target="_blank" rel="noopener noreferrer" className="text-white mx-3 my-1">LinkedIn</a>
-        <a href="https://stackoverflow.com/users/username" target="_blank" rel="noopener noreferrer" className="text-white mx-3 my-1">Stack Overflow</a>
+        <a href="https://stackoverflow.com" target="_blank" rel="noopener noreferrer" className="text-white mx-3 my-1">Stack Overflow</a>
       </div>
     </div>
   </footer>
